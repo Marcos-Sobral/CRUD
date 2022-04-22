eb9bbad670db64d9b1e2dd4c71ca2b175cb1be1e
Marcos Sobral <vini15oficial@gmail.com>
2022-04-22 01:03:28 -0300

    bleeze instalado

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,12 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JogosController;
 use App\Http\Livewire\Header;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::prefix('jogos')->group(function(){
-    Route::get('/', [JogosController::class, 'index'])->name('jogos-index');
+    Route::get('/index', [JogosController::class, 'index'])->name('jogos-index');
     Route::get('/login', [JogosController::class, 'login'])->name('jogos-login');
     Route::get('/grid', [JogosController::class, 'grid'])->name('jogos-grid');
     Route::get('/teste', Header::class);
@@ -31,16 +17,13 @@ Route::prefix('jogos')->group(function(){
     Route::delete('/{id}', [JogosController::class, 'destroy'])->where('id', '[0-9]+')->name('jogos-destroy');
 });
 
-Route::fallback(function(){
-    return 'Erro';
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
-//Route::get('/jogos', [JogosController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-//Route::view('/jogo', 'jogos', ['name' => 'Debora']);
-
-/*Route::get('/jogos/{id?}/{name?}', function($id = null, $name = null) {
-    return view('jogos',['id' => $id, 'names'=>$name]);
-})->where(['id' => '[0-9]+','name' => '[a-z]+']);
-*/
-
+require __DIR__.'/auth.php';
